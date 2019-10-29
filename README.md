d5e3d9437878703981f85d7cd756ef15997ea026
RetroCamara42 <juanmanuel.04@hotmail.com>
2019-10-29 16:45:24 -0500

    Trainer changed to full trainer and new file trainer test various models. README and .gitignore changed too

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # digit_recognizer
-Use of deep learning to classify image of numbres from 0 to 9
+Use of deep learning to classify image of numbers from 0 to 9
+
+Libraries:
+- Numpy 1.16.2
+- Pandas 0.24.2
+- Scikit-learn 0.20.3
+- Keras 2.2.4
+- MatPlotLib 3.0.3
--- src/digit_recognizer_full_trainer.py
+++ src/digit_recognizer_full_trainer.py
@@ -1,29 +1,17 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from tensorflow.python import keras
 from keras.models import Sequential, model_from_json
 from keras.layers import Dense, Flatten, Conv2D, Dropout, MaxPool2D
 from keras.callbacks import ReduceLROnPlateau
 from keras.preprocessing.image import ImageDataGenerator
 
-# Initial variables
+# 42k images of 28x28
 img_rows, img_cols = 28, 28
 num_classes = 10
 
 train_file = "./input/digits/train.csv"
 raw_data = pd.read_csv(train_file)
-
-
-# Model counting
-model_count = 0
-with open('./evaluations/model_count.txt', 'r') as count_file:
-    model_count = int(count_file.read())
-
-with open('./evaluations/model_count.txt', 'w') as count_file:
-    count_file.write(str(model_count+1))
-
 
 # Preparing the data
 y = keras.utils.to_categorical(raw_data.label, num_classes)
@@ -32,11 +20,8 @@ x_as_array = raw_data.values[:,1:]
 x_shaped_array = x_as_array.reshape(num_images, img_rows, img_cols, 1)
 x = x_shaped_array / 255
 
-x_train, x_val_, y_train, y_val_ = train_test_split(
+x_train, x_val, y_train, y_val = train_test_split(
     x, y, test_size=0.3)
-x_val, x_test, y_val, y_test = train_test_split(
-    x_val_, y_val_, test_size=0.3)
-
 
 # The model
 model = Sequential()
@@ -56,14 +41,15 @@ model.add(Dropout(0.2))
 model.add(Dense(num_classes, activation='softmax'))
 
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer='adam',
-              metrics=['accuracy'])
-
 generator = ImageDataGenerator(zoom_range = 0.2,
                             height_shift_range = 0.2,
                             width_shift_range = 0.2,
                             rotation_range = 15)
+
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer='adam',
+              metrics=['accuracy'])
 
 # Annealer
 learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',
@@ -75,51 +61,20 @@ learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',
 
 
 # Training the model
-history = model.fit_generator(generator.flow(x_train,y_train,batch_size=75),
-          epochs=5,
-	      steps_per_epoch = 350,
+model.fit_generator(generator.flow(x_train,y_train,batch_size=75),
+          epochs=50,
+	      steps_per_epoch = 392,
 	      verbose = 1,
           validation_data = generator.flow(x_val,y_val,batch_size=63),
 	      validation_steps = 200,
 	      callbacks=[learning_rate_reduction])
 
-scores = model.evaluate(x_test, y_test, verbose=0)
+scores = model.evaluate(x, y, verbose=0)
 print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 # Saving the model
 model_json = model.to_json()
-with open("./model/model_digit" + str(model_count) + ".json", "w") as json_file:
+with open("./model/model_digit.json", "w") as json_file:
     json_file.write(model_json)
 
-model.save_weights("./model/model_digit" + str(model_count) + ".h5")
-
-# Saving accuracies
-with open('./evaluations/model' + str(model_count) + '.txt', 'w') as accuracy_file:
-    accuracy_file.write('Accuracies:\n')
-    accuracy_file.write(str(history.history['acc']) + '\n')
-    accuracy_file.write('Accuracies from Validation:\n')
-    accuracy_file.write(str(history.history['val_acc']) + '\n')
-    accuracy_file.write('Losses:\n')
-    accuracy_file.write(str(history.history['loss']) + '\n')
-    accuracy_file.write('Losses from Validation:\n')
-    accuracy_file.write(str(history.history['val_loss']))
-    accuracy_file.write('\nmodel' + str(model_count) + ': ' + str(scores[1]))
-
-
-# Plotting accuracy history
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('accuracies')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train','validation'])
-plt.show()
-
-# Plotting loss
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('losses')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train','validation'])
-plt.show()
+model.save_weights("./model/model_digit.h5")
